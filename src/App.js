7515f52572178294d8c46a5e5cb62b20012bf8f9
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-10-06 17:58:17 +0100

    - added navbar - added componetes folder - added image folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import NavBar from "./componentes/navBar";
 
 function App() {
-  return <>tati</>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
