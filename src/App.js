21c8ffe61acda418fec0097b99c5ed3389a948eb
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-10-15 13:30:01 +0100

    - added footer in the page - style the footer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./App.css";
+import "./App.css"; // Import your CSS file
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NavBar from "./componentes/navBar";
+import Footer from "./componentes/footer";
 
 import Home from "./pages/home";
 import AboutUs from "./pages/aboutus";
+
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        {/* ... Add your other routes here */}
-      </Routes>
+      <div>
+        <NavBar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ src/componentes/footer.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Logo from "../images/logo.png";
+import { FaLinkedin, FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        background: "#00252A",
+        padding: "20px 0",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <div className="container mt-5 ">
+        <div className="row">
+          <div className="col-md-4">
+            <img className="LogoOne" src={Logo} alt="Logo" />
+            <p className="text-light">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+            <div className="social-icons">
+              <a href="#" className="btn btn-outline-light icon-background">
+                <FaFacebook className="icon" />
+              </a>
+              <a href="#" className="btn btn-outline-light icon-background">
+                <FaInstagram className="icon" />
+              </a>
+              <a href="#" className="btn btn-outline-light icon-background">
+                <FaTwitter className="icon" />
+              </a>
+              <a href="#" className="btn btn-outline-light icon-background">
+                <FaLinkedin className="icon" />
+              </a>
+            </div>
+          </div>
+          <div className="col-md-4 text-light">
+            <h3>Links</h3>
+            <ul className="list-unstyled">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Who we are</li>
+              <li>What we Do?</li>
+              <li>Solution</li>
+              <li>Contact US</li>
+            </ul>
+          </div>
+          <div className="col-md-4 text-light">
+            <h3>Contact us</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+            <p>+923183561921</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <hr
+          className="footerHr text-white-50 "
+          style={{
+            width: "95%",
+            height: "1px",
+            marginLeft: "30px",
+          }}
+        />
+        <p className="text-white-50 text-center">
+          © 2023 Copyright by IK Developers. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
